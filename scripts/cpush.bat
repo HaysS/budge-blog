286b403147cec6d94b9229b724752f2533b7e594
HaysS <stanfordhays@gmail.com>
2018-10-07 15:15:29 -0500

    added new script section

---
+++ scripts/cpush.bat
@@ -0,0 +1,5 @@
+@echo off
+:: This is a batch file to make git is faster to commit and push git changes from the cmd line
+:: echo Type your git commit description:
+echo This script is under construction and does not work.
+pause
